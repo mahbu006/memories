f46d97da31bd00c05f961c4b92d0310ef7ed1ad1
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-01-07 11:55:50 -0600

    Updates to index.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,6 +1,5 @@
-import * as api from '../api';
+import * as api from '../api/index.js';
 
-// Action Creators
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
@@ -9,4 +8,14 @@ export const getPosts = () => async (dispatch) => {
     } catch (error) {
         console.log(error.message);
     }
-}
+};
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
