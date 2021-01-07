24e4f78a14b94e2772fa6c716c3bc256e42fb507
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-01-07 00:50:18 -0600

    Added Form functionality

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://raiyanmahbub:raiyanmahbub123@cluster0.8lrc9.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
