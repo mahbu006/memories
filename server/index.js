69f57d2fffe75181f621f9b00b50415b11009287
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-01-06 20:07:40 -0600

    Initial npm commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://raiyanmahbub:raiyanmahbub123@cluster0.8lrc9.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
