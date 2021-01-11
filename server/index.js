6fd330099b5b6635502441f23317781cdae57b1d
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-01-10 23:44:50 -0600

    updates to index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello to Memories API');
+    res.send('Hello to the Memories API');
 })
 
 const PORT = process.env.PORT || 5000;
