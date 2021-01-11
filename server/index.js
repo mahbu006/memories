4e656620451fac5ce5e19574c22df5619538d09a
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-01-11 00:18:44 -0600

    Before reset

--- server/index.js
+++ server/index.js
@@ -15,9 +15,14 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello to the Memories API');
+})
+
+const CONNECTION_URL = "mongodb+srv://raiyanmahbub:raiyanmahbub123@cluster0.8lrc9.mongodb.net/<dbname>?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
